2546d39e6089810bbfcdc07ecb08d62475f51d53
kyrylozhadan <kzhadan02@gmail.com>
2025-03-20 23:37:11 +0100

    hw 3.3

---
+++ hw3.3.py
@@ -0,0 +1,15 @@
+# lst = [1, 2, 3, 4, 5, 6] #=> [[1, 2, 3], [4, 5, 6]]
+# lst = [1, 2, 3]# => [[1, 2], [3]]
+# lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] #=> [[1, 2, 3], [4, 5]]
+# lst = [1] #=> [[1], []]
+lst = [] #=> [[], []]
+length = len(lst)
+if not length % 2 == 0:
+    mid = int(length / 2 + 1)
+else:
+    mid = int(length / 2)
+lst_separated = [lst[0:mid], lst[mid:length]]
+print(lst_separated)
+
+
+
