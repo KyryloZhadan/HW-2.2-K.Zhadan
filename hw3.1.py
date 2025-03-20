1a8744016cadac0b1d3fd9bcc0ba75f357f4aad7
kyrylozhadan <kzhadan02@gmail.com>
2025-03-20 22:40:51 +0100

    hw 3.1

---
+++ hw3.1.py
@@ -0,0 +1,16 @@
+number_1 = float(input('Enter first number:'))
+number_2 = float(input('Enter second number:'))
+action = input('Select an action:')
+if action == "+":
+    print(number_1 + number_2)
+elif action == "-":
+    print(number_1 - number_2)
+elif action == "/":
+    if number_2 !=0:
+        print(number_1 / number_2)
+    elif number_2 == 0:
+        print('Why are u trying to divide by zero?! Run script again and try with normal numbers.')
+elif action == "*":
+    print(number_1 * number_2)
+else:
+    print('Unknown action')
